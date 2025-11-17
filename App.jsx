08dd722ad967b3d0fe71924d74beee7f7a895733
Santiago22-py <joaovitor.santiagovianamelo@edu.sait.ca>
2025-11-17 15:22:57 -0700

    Made changes for lab 2

--- App.jsx
+++ App.jsx
@@ -4,15 +4,22 @@
  * @format
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView,} from 'react-native'; //Made it a single line since there is only one import
 import ToDoList from './src/components/ToDoList';
 import ToDoForm from './src/components/ToDoForm';
 
 function App() {
+
+  const [tasks, setTasks] = useState([
+    'Do laundry',
+    'Go to gym',
+    'Walk dog'
+  ]);
+
   return (
     <SafeAreaView>
-      <ToDoList />
+      <ToDoList tasks={tasks}/>
       <ToDoForm />
     </SafeAreaView>
   );
--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -8,33 +8,22 @@ import {
 } from 'react-native';
 
 // ToDoList Component
-export default function ToDoList()
+export default function ToDoList({ tasks })
 {   
     // Render the list of tasks
     return (
         
         <ScrollView> {/* Scrollable list of tasks */}
             {/* Task 1 */}
-            <Pressable>
-                <View style={[styles.task, styles.completed]}>
-                    <Text style={styles.taskText}>Do laundry</Text>
-                </View>
-            </Pressable>
-        
-            {/* Task 2 */}
-            <Pressable>
-                <View style={[styles.task]}>
-                    <Text style={styles.taskText}>Go to gym</Text>
-                </View>
-            </Pressable>
-
-            {/* Task 3 */}
-            <Pressable>
-                <View style={[styles.task, styles.completed]}>
-                    <Text style={styles.taskText}>Walk dog</Text>
-                </View>
-            </Pressable>
+            {tasks.map((task, index) => (
+                <Pressable key={index}>
+                    <View style={styles.task}>
+                        <Text style={styles.taskText}>{task}</Text>
+                    </View>
+                </Pressable>
+            ))}
         </ScrollView>
+        
     );
 }   
     // Styles for the ToDoList component
