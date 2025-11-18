29c784d4c2d3cb18add75f626e08ead6ce1736d4
Santiago22-py <joaovitor.santiagovianamelo@edu.sait.ca>
2025-11-18 16:01:18 -0700

    Removed comments from toDoList file

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,46 +1,31 @@
 import React from 'react';
-import {
-  StyleSheet,
-  Pressable,
-  View,
-  Text,
-  ScrollView,
-} from 'react-native';
+import { StyleSheet, Pressable, View, Text, ScrollView } from 'react-native';
 
 // ToDoList Component
-export default function ToDoList({ tasks })
-{   
-    // Render the list of tasks
-    return (
-        
-        <ScrollView> {/* Scrollable list of tasks */}
-            {/* Task 1 */}
-            {tasks.map((task, index) => (
-                <Pressable key={index}>
-                    <View style={styles.task}>
-                        <Text style={styles.taskText}>{task}</Text>
-                    </View>
-                </Pressable>
-            ))}
-        </ScrollView>
-        
-    );
-}   
-    // Styles for the ToDoList component
-    const styles = StyleSheet.create
-    ({
-        task: 
-        {
-            padding: 10,
-            borderBottomWidth: 1,
-            borderColor: '#ccc',
-        },
-        completed: 
-        {
-            backgroundColor: '#e0e0e0',
-        },
-        taskText: 
-        {
-            fontSize: 16,
-        },
-    });
+export default function ToDoList({ tasks }) {
+  // Render the list of tasks
+  return (
+    <ScrollView>{tasks.map((task, index) => (
+        <Pressable key={index}>
+          <View style={styles.task}>
+            <Text style={styles.taskText}>{task}</Text>
+          </View>
+        </Pressable>
+      ))}
+    </ScrollView>
+  );
+}
+// Styles for the ToDoList component
+const styles = StyleSheet.create({
+  task: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
+  },
+  completed: {
+    backgroundColor: '#e0e0e0',
+  },
+  taskText: {
+    fontSize: 16,
+  },
+});
